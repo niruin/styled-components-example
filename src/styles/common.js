3ca4bd80530812b15268e562f2b981fd1eed6308
qq <nftless@gmail.com>
2021-07-21 13:39:54 +0300

    feat: add styles

---
+++ src/styles/common.js
@@ -0,0 +1,27 @@
+export const colors = {
+  white: '#FFFFFF',
+  blue: '#23527c',
+  gray: '#8899a6',
+  black: '#292f33',
+  mediumGray: '#959595',
+  lightGray: '#8899a6',
+  superLightGray: '#F1F5F8'
+};
+
+const deviceSize = {
+  mobileS: 320,
+  mobileM: 480,
+  mobileL: 600,
+  tablet: 768,
+  laptop: 992,
+  desktop: 1280
+};
+
+export const device = {
+  mobileS: `(max-width: ${deviceSize.mobileS}px)`,
+  mobileM: `(max-width: ${deviceSize.mobileM}px)`,
+  mobileL: `(max-width: ${deviceSize.mobileL}px)`,
+  tablet: `(max-width: ${deviceSize.tablet}px)`,
+  laptop: `(max-width: ${deviceSize.laptop}px)`,
+  desktop: `(max-width: ${deviceSize.desktop}px)`
+};
---
+++ src/styles/global-style.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import RobotoThin from './fonts/roboto/Roboto-Thin.ttf';
+import RobotoRegular from './fonts/roboto/Roboto-Regular.ttf';
+import Merriweather from './fonts/merriweather/Merriweather-LightItalic.ttf';
+import MerriweatherRegular from './fonts/merriweather/Merriweather-Regular.ttf';
+import { colors } from './common';
+
+function fontFace (name, src, fontWeight = 'normal', fontStyle = 'normal') {
+  return `
+     @font-face {
+      font-family: ${name};
+      src: url(${src}) format("truetype");
+      font-weight: ${fontWeight};
+      font-style: ${fontStyle};
+    }
+  `;
+}
+
+export const GlobalStyle = createGlobalStyle`
+  ${fontFace('Roboto', RobotoRegular)}
+  ${fontFace('MerriweatherRegular', MerriweatherRegular)}
+  ${fontFace('RobotoThin', RobotoThin, 'normal', 'italic')}
+  ${fontFace('Merriweather', Merriweather, 'normal', 'italic')}
+
+  body {
+    margin: 0;
+    color: ${colors.black};
+    font-family: MerriweatherRegular, serif;
+  }
+`;
