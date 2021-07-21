414204103384db1a23597f569f4c3ca8a22d5102
qq <nftless@gmail.com>
2021-07-21 14:53:36 +0300

    feat: edit src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { App } from './app/app';
+
+import { history } from './common/routing';
+import { GlobalStyle } from './styles/global-style';
+
+function Root () {
+  return (
+    <>
+      <GlobalStyle/>
+      <Router history={history}>
+        <App/>
+      </Router>
+    </>
+  );
+}
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
