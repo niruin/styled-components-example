e4edfb916d5365afa3f66602ef4855d9aeea4ca1
qq <nftless@gmail.com>
2021-10-10 22:37:25 +0300

    refactor(index.js): remove not used import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import {App} from './app/app';
