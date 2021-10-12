bc6d02eebd84c481ea87af765dd3d428701bf57c
qq <nftless@gmail.com>
2021-10-12 17:39:20 +0300

    feat(pages): add some content on page

--- src/common/components/link-button/link-button.js
+++ src/common/components/link-button/link-button.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import {colors, device} from '../../../styles/common';
 
 export const LinkButton = styled.div`
--- src/pages/pages/components/page/page.js
+++ src/pages/pages/components/page/page.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import {LinkButton} from '../../../../common/components/link-button';
+import {Container} from '../../../../common/components/container';
+import {history} from '../../../../common/routing';
+
+const Root = styled.div`
+  padding: 80px 0 50px;
+`;
 
 export function Page() {
-  return <div>not content</div>;
+
+  const redirectPage = path => {
+    history.push(path);
+  };
+  return (
+    <Root>
+      <Container>
+        <LinkButton onClick={() => redirectPage('/')}>Home</LinkButton>
+        <LinkButton onClick={() => redirectPage('/about')}>About us</LinkButton>
+        <LinkButton onClick={() => redirectPage('/services')}>Services</LinkButton>
+        <LinkButton onClick={() => redirectPage('/portfolio')}>Portfolio</LinkButton>
+        <LinkButton onClick={() => redirectPage('/blog')}>Blog</LinkButton>
+        <LinkButton onClick={() => redirectPage('/contact')}>Contact us</LinkButton>
+      </Container>
+    </Root>
+  );
 }
