284e12976df5df34e3bad0d26e8bb49d342ea010
qq <nftless@gmail.com>
2021-10-21 18:04:14 +0300

    refactor

--- src/common/components/container/container.js
+++ src/common/components/container/container.js
@@ -6,7 +6,6 @@ import {device} from '../../../styles/common';
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  width: 100%;
   max-width: 1280px;
   margin-left: auto;
   margin-right: auto;
