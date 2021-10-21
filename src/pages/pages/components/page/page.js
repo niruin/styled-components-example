8e9cf05601516889b4e063d3f76d04a4a69b7dab
qq <nftless@gmail.com>
2021-10-21 18:05:40 +0300

    fix(pages): layout page

--- src/pages/pages/components/page/page.js
+++ src/pages/pages/components/page/page.js
@@ -4,10 +4,35 @@ import styled from 'styled-components';
 import {LinkButton} from '../../../../common/components/link-button';
 import {Container} from '../../../../common/components/container';
 import {history} from '../../../../common/routing';
+import {SectionHeader} from '../../../../common/components/section-header';
+import {device} from '../../../../styles/common';
 
 const Root = styled.div`
   padding: 80px 0 50px;
 `;
+
+const ContainerStyled = styled(Container)`
+  display: block;
+`;
+
+const LinkButtonStyled = styled(LinkButton)`
+  text-align: left;
+  margin-left: 0;
+  padding-left: 0;
+`;
+
+const WrapperLinkButtons = styled.div`
+  display: flex;
+
+  @media ${device.tablet} {
+    flex-direction: column;
+  }
+
+  @media ${device.mobileS} {
+    padding-left: 10px;
+    padding-right: 10px;
+  } ;
+`
 
 export function Page() {
 
@@ -16,14 +41,17 @@ export function Page() {
   };
   return (
     <Root>
-      <Container>
-        <LinkButton onClick={() => redirectPage('/')}>Home</LinkButton>
-        <LinkButton onClick={() => redirectPage('/about')}>About us</LinkButton>
-        <LinkButton onClick={() => redirectPage('/services')}>Services</LinkButton>
-        <LinkButton onClick={() => redirectPage('/portfolio')}>Portfolio</LinkButton>
-        <LinkButton onClick={() => redirectPage('/blog')}>Blog</LinkButton>
-        <LinkButton onClick={() => redirectPage('/contact')}>Contact us</LinkButton>
-      </Container>
+      <ContainerStyled>
+        <SectionHeader>Pages</SectionHeader>
+        <WrapperLinkButtons>
+          <LinkButtonStyled onClick={() => redirectPage('/')}>Home</LinkButtonStyled>
+          <LinkButtonStyled onClick={() => redirectPage('/about')}>About us</LinkButtonStyled>
+          <LinkButtonStyled onClick={() => redirectPage('/services')}>Services</LinkButtonStyled>
+          <LinkButtonStyled onClick={() => redirectPage('/portfolio')}>Portfolio</LinkButtonStyled>
+          <LinkButtonStyled onClick={() => redirectPage('/blog')}>Blog</LinkButtonStyled>
+          <LinkButtonStyled onClick={() => redirectPage('/contact')}>Contact us</LinkButtonStyled>
+        </WrapperLinkButtons>
+      </ContainerStyled>
     </Root>
   );
 }
